a541698f27f3f1a0b428f9574d1edcabb75e0388
pickyko <68360714+pickyko@users.noreply.github.com>
2023-06-05 10:48:41 -0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+import sys
+sys.path.insert(0, 'utils')
+from musixmatch import musixmatch
+from animelyrics import animelyrics
+from vocaloidlyrics import vocaloidlyrics
+from genius import genius
+from note import note
+
 # path to all your mp3 files
 music_dir = ""
 # use geckodriver
@@ -5,5 +13,16 @@ driver_path = ""
 # path to xpi file of ublock origin.  If you don't have put False as a boolean.
 ublock_path = ""
 
+# if you want the program to ask you every time a website should be used
 interactive_mode = True
+# if you want to search for lyrics even though lyrics already exist in the tag
 overwrite_existing = False
+
+# website order
+search_order = [
+musixmatch,
+animelyrics,
+vocaloidlyrics,
+genius,
+note
+]
